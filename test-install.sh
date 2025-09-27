09ef92e861bd61b062ad630887a6608a95f506b1
megvadulthangya <67601373+megvadulthangya@users.noreply.github.com>
2025-09-27 12:03:23 +0200

    Update test-install.sh

--- test-install.sh
+++ test-install.sh
@@ -98,7 +98,7 @@ install_aur() {
 REPO_PKGS=(
   dmenu rofi flameshot picom scrot unclutter xorg-xbacklight alsa-utils
   mpd mpc playerctl xorg-fonts-misc ttf-roboto woff2-font-awesome
-  arandr lxappearance lxqt-policykit xorg-xev terminus-font xss-lock wavemon
+  arandr lxappearance lxqt-policykit xorg-xev terminus-font xss-lock wavemon network-manager-applet
 )
 
 echo "Repo csomagok telepítése..."
