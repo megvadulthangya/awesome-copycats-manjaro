74b75415b69791fec3c13c746b5fa7c205828a65
megvadulthangya <67601373+megvadulthangya@users.noreply.github.com>
2025-09-27 12:02:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -51,7 +51,7 @@ install_aur() {
 REPO_PKGS=(
   dmenu rofi flameshot picom scrot unclutter xorg-xbacklight alsa-utils
   mpd mpc playerctl xorg-fonts-misc ttf-roboto woff2-font-awesome
-  arandr lxappearance lxqt-policykit xorg-xev terminus-font xss-lock
+  arandr lxappearance lxqt-policykit xorg-xev terminus-font xss-lock network-manager-applet
 )
 
 echo "Repo csomagok telepítése..."
